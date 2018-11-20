151fec94fddfa061e98c4e89334af3a622c5ed22
Anna <41161588+Bashanka@users.noreply.github.com>
2018-11-20 18:04:46 +0400

    Add files via upload
    Minesweeper

---
+++ src/abstaractinterface/MinesweeperDemo.java
@@ -0,0 +1,11 @@
+package abstaractinterface;
+
+public class MinesweeperDemo {
+
+    public static void main(String[] args) {
+        Minesweeper minesweeper = new Minesweeper();
+        minesweeper.drawShowBoard();
+        minesweeper.drawBoard();
+    }
+
+}
